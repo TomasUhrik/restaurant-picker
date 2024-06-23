e2920196e30bd38f9ed7e7f10951ba0d5e1d86c7
Tomas Uhrik <tomasuhrik@ondaytrip.com>
2024-06-23 12:21:59 +0900

    chore: testing CI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Testing CI</h1>
+
       <div>
         <h1>Home</h1>
         <Link href="/about">About</Link>
