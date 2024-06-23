bf5d7ac9315261644580e0be1a6e71e8a3826f98
Tomas Uhrik <tomasuhrik@ondaytrip.com>
2024-06-23 13:58:46 +0900

    chore: test react server component on production

--- src/app/components/place/place.tsx
+++ src/app/components/place/place.tsx
@@ -1,3 +1,5 @@
+const COGENT_LABS_LL = "35.6647034,139.7376358";
+
 export async function Place() {
   const options = {
     method: "GET",
@@ -7,17 +9,26 @@ export async function Place() {
     },
   };
 
+  const queryParams = new URLSearchParams({
+    ll: COGENT_LABS_LL,
+    radius: "1000",
+  }).toString();
+
+  // @TODO: Typings
   const result = await fetch(
-    "https://api.foursquare.com/v3/places/search",
+    "https://api.foursquare.com/v3/places/search?" + queryParams,
     options
   )
     .then((response) => response.json())
     .catch((err) => console.error(err));
 
+  const randomIndex = Math.floor(Math.random() * result.results.length);
+  const selected = result && result.results[randomIndex];
+
   return (
     <div>
       <h1>Place</h1>
-      <p>{result && result.results[0].name}</p>
+      <p>{selected.name}</p>
     </div>
   );
 }
