b0283aa23ad6ae1dbd6c6a04e32940e57758bf46
Tomas Uhrik <tomasuhrik@ondaytrip.com>
2024-06-24 14:46:48 +0900

    chore: improve typing

--- src/data-access/fetch-random-place.ts
+++ src/data-access/fetch-random-place.ts
@@ -4,14 +4,19 @@ import { RANDOM_PLACE_TAG } from "@/consts/cache-tags";
 
 const COGENT_LABS_LL = "35.6647034,139.7376358";
 
+const NO_STORE = "no-store";
+
+type Place4S = {
+  name: string;
+};
+
 export async function fetchRandomPlace() {
   const queryParams = new URLSearchParams({
     ll: COGENT_LABS_LL,
     radius: "1000",
   }).toString();
 
-  // @TODO: Typings
-  const result = await fetch(
+  const result: Array<Place4S> = await fetch(
     "https://api.foursquare.com/v3/places/search?" + queryParams,
     {
       method: "GET",
@@ -19,18 +24,18 @@ export async function fetchRandomPlace() {
         accept: "application/json",
         Authorization: process.env.FOUR_SQUARE_API_KEY || "",
       },
-      // @TODO: Type casting
-      cache: "no-store" as RequestCache,
+      cache: NO_STORE,
       next: {
         tags: [RANDOM_PLACE_TAG],
       },
     }
   )
     .then((response) => response.json())
+    .then((data) => data.results)
     .catch((err) => console.error(err));
 
-  const randomIndex = Math.floor(Math.random() * result.results.length);
-  const selected = result && result.results[randomIndex];
+  const randomIndex = Math.floor(Math.random() * result.length);
+  const selected = result && result[randomIndex];
 
   return selected;
 }
