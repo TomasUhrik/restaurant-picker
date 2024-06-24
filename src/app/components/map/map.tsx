80526e59d44c251f05bfb27cc40021d16c19df48
Tomas Uhrik <tomasuhrik@ondaytrip.com>
2024-06-24 18:06:31 +0900

    feat: Show path to venue

--- src/app/components/map/map.tsx
+++ src/app/components/map/map.tsx
@@ -3,14 +3,6 @@
 import { COGENT_LABS_LATITUDE, COGENT_LABS_LONGITUDE } from "@/consts/map";
 import GoogleMapReact from "google-map-react";
 
-const AnyReactComponent = ({
-  text,
-}: {
-  text: string;
-  lat: number;
-  lng: number;
-}) => <h1>{text}</h1>;
-
 export type VenueCoords = { lat: number; lng: number };
 
 export const Map = ({ venueCoords }: { venueCoords?: VenueCoords }) => {
@@ -23,7 +15,6 @@ export const Map = ({ venueCoords }: { venueCoords?: VenueCoords }) => {
   };
 
   return (
-    // Important! Always set the container height explicitly
     <div style={{ height: "100vh", width: "100%" }}>
       <GoogleMapReact
         bootstrapURLKeys={{
@@ -31,15 +22,29 @@ export const Map = ({ venueCoords }: { venueCoords?: VenueCoords }) => {
         }}
         defaultCenter={defaultProps.center}
         defaultZoom={defaultProps.zoom}
-      >
-        {venueCoords && (
-          <AnyReactComponent
-            lat={venueCoords.lat}
-            lng={venueCoords.lng}
-            text="Cogent labs office"
-          />
-        )}
-      </GoogleMapReact>
+        yesIWantToUseGoogleMapApiInternals
+        onGoogleApiLoaded={({ map, maps }) => {
+          const directionsService = new maps.DirectionsService();
+
+          directionsService.route(
+            {
+              origin: new maps.LatLng(
+                COGENT_LABS_LATITUDE,
+                COGENT_LABS_LONGITUDE
+              ),
+              destination: new maps.LatLng(venueCoords?.lat, venueCoords?.lng),
+              travelMode: maps.TravelMode.WALKING,
+            },
+            (result: any) => {
+              // @TODO: Do this better
+              new maps.DirectionsRenderer({
+                map,
+                directions: result,
+              });
+            }
+          );
+        }}
+      />
     </div>
   );
 };
