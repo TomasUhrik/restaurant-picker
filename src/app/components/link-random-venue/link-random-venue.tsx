67244c807ae8fc04fdb0f3df6fa6a7d2019e472c
Tomas Uhrik <uhrik.tom@gmail.com>
2024-08-10 13:51:54 +0900

    feat: Link random venue

--- src/app/components/link-random-venue/link-random-venue.tsx
+++ src/app/components/link-random-venue/link-random-venue.tsx
@@ -1,18 +1,26 @@
-import { buttonVariants } from "@/components/ui/button";
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { fetchRandomPlaceId } from "@/data-access/fetch-random-place";
 import { cn } from "@/lib/utils";
 import { Dice6Icon } from "lucide-react";
-import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export const LinkRandomVenue = () => {
+  const router = useRouter();
+
+  const handleOnClick = async () => {
+    const randomPlaceId = await fetchRandomPlaceId();
+
+    router.push(`/place/${randomPlaceId}`);
+  };
+
   return (
-    <Link
-      className={buttonVariants({ variant: "default", size: "sm" })}
-      href="/"
-    >
+    <Button size={"sm"} variant={"default"} onClick={handleOnClick}>
       <span className={cn("mr-1")}>
         <Dice6Icon size={18} />
       </span>
       Randomize
-    </Link>
+    </Button>
   );
 };
